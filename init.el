3b282bb9a2ba876e90b2affaf2fb00dc5ef71cba
mega-haste <www.hichamhot@gmail.com>
2025-01-14 18:39:19 +0100

    Haste

--- init.el
+++ init.el
@@ -6,6 +6,7 @@
 ; bootstraping Straight.el
 (load-file "~/.emacs.d/bootstrap.el")
 
+(add-to-list 'load-path "~/.emacs.d/lisp/")
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
@@ -15,16 +16,9 @@
 (load "functions")
 (load "launcher")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'haste-mode)
+
+(custom-set-variables)
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(dashboard-heading ((t (:foreground "cyan" :weight bold :height 0.8)))))
